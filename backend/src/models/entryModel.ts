51d38e4c6a9e021bdb5e16e47a9c26f11d351d6f
axizkhan <axizkhan786110@gmail.com>
2026-03-02 10:01:42 +0530

    chors/add interface user in global Request type declaratio

--- backend/src/models/entryModel.ts
+++ backend/src/models/entryModel.ts
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 const EntrySchema = new Schema({
   lenderId: { type: Schema.Types.ObjectId, ref: "User" },
   borowerId: { type: Schema.Types.ObjectId, ref: "User" },
-  expenseId: { type: Schema.Types.ObjectId, ref: "Expense" },
+  expenseId: { type: Schema.Types.ObjectId, ref: "Expense", default: null },
+  paymentId: { type: Schema.Types.ObjectId, ref: "Payment", default: null },
   groupId: { type: Schema.Types.ObjectId, ref: "Group" },
   type: { type: String, enum: ["expense", "payment"] },
   amount: {
